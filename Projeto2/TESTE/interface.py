a524214d334ed39591f4d723b4140aeb4699d4d1
Gabriel <witthoftf13@gmail.com>
2023-07-20 14:56:38 -0300

    Correcao Interface

--- Projeto2/TESTE/interface.py
+++ Projeto2/TESTE/interface.py
@@ -19,18 +19,18 @@ class Interface:
         # Configuração da janela principal
         self.janela = tk.Tk()
         self.janela.title("Gerenciador de Produtos")
-        self.janela.geometry("600x750")
+        self.janela.geometry("660x800")
 
         # Divisão da janela em duas partes
         panedwindow = tk.PanedWindow(self.janela, orient=tk.HORIZONTAL)
         panedwindow.pack(fill=tk.BOTH, expand=True)
 
         # Frame da esquerda (300x800)
-        frame_esquerda = tk.Frame(panedwindow, width=300, height=800)
+        frame_esquerda = tk.Frame(panedwindow, width=330, height=800)
         panedwindow.add(frame_esquerda)
 
         # Frame do período
-        frame_periodo = tk.Frame(frame_esquerda)
+        frame_periodo = tk.Frame(frame_esquerda, highlightthickness=1, highlightbackground="black")
         frame_periodo.grid(row=0, column=0, padx=10, pady=10)
 
         # Label status período
@@ -45,7 +45,7 @@ class Interface:
         botao_finalizar.grid(row=1, column=1, padx=10, pady=10)
 
         # Frame de cadastro de produtos
-        frame_cadastro = tk.Frame(frame_esquerda)
+        frame_cadastro = tk.Frame(frame_esquerda, highlightthickness=1, highlightbackground="black")
         frame_cadastro.grid(row=1, column=0, padx=10, pady=10)
 
         # Campos de cadastro de produto
@@ -84,19 +84,12 @@ class Interface:
         self.lista_produtos = tk.Listbox(frame_produtos, height=20, width=40)
         self.lista_produtos.grid(row=1, column=0, columnspan=2, padx=10, pady=5)
 
-        #self.botao_editar = tk.Button(frame_produtos, text="Editar", width=15, command=self.produto.editar_produto)
-        #self.botao_editar.grid(row=2, column=1, padx=10, pady=10)
-
-        # Botão Vendas
-        self.botao_vendas = tk.Button(frame_produtos, text="Vendas", width=15, command=self.estrutura.registrar_venda)
-        self.botao_vendas.grid(row=2, column=0, padx=10, pady=10)
-
          # Frame da direita (300x800)
-        frame_direita = tk.Frame(panedwindow, width=300, height=800)
+        frame_direita = tk.Frame(panedwindow, width=330, height=800)
         panedwindow.add(frame_direita)
 
          # Frame de edição de produtos
-        frame_edicao = tk.Frame(frame_direita)
+        frame_edicao = tk.Frame(frame_direita, highlightthickness=1, highlightbackground="black")
         frame_edicao.grid(row=0, column=0, padx=10, pady=10, sticky=tk.W)
 
         # Label "Edição de Produto"
@@ -147,6 +140,33 @@ class Interface:
         self.botao_excluir = tk.Button(frame_edicao, text="Excluir", width=15, command=self.produto.excluir_produto)
         self.botao_excluir.grid(row=7, column=0, padx=10, pady=10)
 
+        # Frame de registro de vendas
+        frame_vendas = tk.Frame(frame_direita, highlightthickness=1, highlightbackground="black")
+        frame_vendas.grid(row=1, column=0, padx=10, pady=10, sticky=tk.W)
+
+        # Label para exibir o nome do frame de registro de vendas
+        label_nome_frame_vendas = tk.Label(frame_vendas, text="Registro de Vendas:")
+        label_nome_frame_vendas.grid(row=0, column=0, columnspan=2, padx=10, pady=10, sticky=tk.W)
+
+        # Combobox de produtos para registro de vendas
+        label_produtos_venda = tk.Label(frame_vendas, text="Produto:")
+        label_produtos_venda.grid(row=1, column=0, padx=10, pady=5, sticky=tk.W)
+
+        self.combobox_produtos_venda = ttk.Combobox(frame_vendas)
+        self.combobox_produtos_venda.grid(row=1, column=1, padx=10, pady=5)
+        self.combobox_produtos_venda.bind("<<ComboboxSelected>>", None)
+
+        # Campo de quantidade para registro de vendas
+        label_quantidade_venda = tk.Label(frame_vendas, text="Quantidade:")
+        label_quantidade_venda.grid(row=2, column=0, padx=10, pady=5, sticky=tk.W)
+
+        self.entry_quantidade_venda = tk.Entry(frame_vendas)
+        self.entry_quantidade_venda.grid(row=2, column=1, padx=10, pady=5)
+
+        # Campo de venda cortesia para registro de vendas
+        self.venda_cortesia_var = tk.IntVar()
+        checkbox_venda_cortesia = tk.Checkbutton(frame_vendas, text="Venda Cortesia", variable=self.venda_cortesia_var)
+        checkbox_venda_cortesia.grid(row=3, column=0, columnspan=2, padx=10, pady=5)
 
         # Configurar evento para fechar o programa
         self.janela.protocol("WM_DELETE_WINDOW", self.fechar_programa)
